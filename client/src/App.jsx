948bc503219c7cc2d32185ac3db6a35a3aa22134
Aman Ranjan <aman.ranjan.529@gmail.com>
2024-03-10 12:48:59 +0530

    Created the Header Section using Flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import { BrowserRouter,Route,Routes  } from 'react-router-dom'
+import Header from './components/Header'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
