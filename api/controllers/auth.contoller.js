73999f0785e7b1e226747e7b9a0a40ebbee1e1b7
Aman Ranjan <aman.ranjan.529@gmail.com>
2024-03-10 15:42:23 +0530

    Creting Middle ware and error Handler function for recognizing errors

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -1,9 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';    
-export const signup = async (req,res)=>{
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req,res,next)=>{
     const {username , email , password} = req.body;
     if(!username || !email || !password || username=== '' || password === '' || email===''){
-        return res.status(400).json({message:'All Fields are required'});
+
+        next(errorHandler(400,'All fields are required'));
     }
 
     const hashPassword = bcryptjs.hashSync(password,10);
@@ -17,7 +19,7 @@ export const signup = async (req,res)=>{
         await newUser.save();
     res.json('signup Successfull');
     } catch (error) {
-        res.status(500).json({message:error.message});
+        next(error);
     }
     
     // console.log(req.body);
